e50a1ebc4f8fd8c3997582b3e71b2c0d6d25e346
werlesson <werlessono@gmail.com>
2021-04-10 17:45:07 -0300

    feat(components): add react-icons

--- src/components/UserInfo/index.jsx
+++ src/components/UserInfo/index.jsx
@@ -1,11 +1,14 @@
 import * as S from './styles'
+import { FiLogOut } from 'react-icons/fi'
 
 const UserInfo = ({ name }) => (
   <S.Wrapper>
     <span>
       Bem vindo, <strong>{name}</strong>!
     </span>
-    <span className="ml-2">ICO</span>
+    <span className="ml-2">
+      <FiLogOut />
+    </span>
   </S.Wrapper>
 )
 
